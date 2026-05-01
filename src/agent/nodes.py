89ad3f3ce7012bccdb000f1334771eb70f7f8b6f
Oleksandr <oleksandr.proshkin1@gmail.com>
2026-05-01 20:27:45 +0200

    refactor: Move db_utils into utils module

--- src/agent/nodes.py
+++ src/agent/nodes.py
@@ -1,7 +1,7 @@
 from typing import Literal
 from config import DatabaseConfig
 from agent.states import AgentState
-from agent.db_utils import (
+from utils.db_utils import (
     get_engine,
     fetch_schema_metadata,
     metadata_to_ddl,
